0b28905eb43c0b2f2f77b7735c3fb75fbb68c72c
lvst1929 <sidibe.mel@gmail.com>
2026-05-13 10:14:27 +0200

    Structure requête API sans assertions

---
+++ frontend/cypress/e2e/1-api/api.cy.js
@@ -0,0 +1,111 @@
+//pour les requêtes nécessitant une connexion préalable utiliser beforeEach
+//séparer les requêtes avec et sans authentification
+//préparer les assertions "vérifications"
+
+describe("Appels API sans authentification", () => {
+    it("doit refuser l'accès au panier sans connexion", () => {
+        cy.request({
+            method: "GET",
+            url: "http://localhost:8081/orders",
+            failOnStatusCode: false
+
+        })
+    })
+
+    it("doit retourner 401 pour un user inconnu", () => {
+        cy.request({
+            method: "POST",
+            url: "http://localhost:8081/login",
+            body: {
+                username: "fauxuser@faux.fr",
+                password: "fauxuser",
+            }
+
+        })
+
+    })
+
+    it("doit retourner la fiche du produit", () => {
+        cy.request({
+            method: "GET",
+            url: "http://localhost:8081/products/2"
+
+        })
+    })
+})
+
+describe("appels API avec authentification", () => {
+    let token
+    beforeEach(() => {
+        cy.request({
+            method: "POST",
+            url: "http://localhost:8081/login",
+            body: {
+                username: "test2@test.fr",
+                password: "testtest"
+            }
+        })
+            .then((response) => {
+                token = response.body.token
+            })
+
+    })
+
+
+    it("doit retourner la liste des produits qui sont dans le panier", () => {
+        cy.request({
+            method: "GET",
+            url: "http://localhost:8081/orders",
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+    })
+
+
+    it("doit ajouter un produit disponible au panier", () => {
+        cy.request({
+            method: "PUT",
+            url: "http://localhost:8081/orders/add",
+            body: {
+                "product": 3,
+                "quantity": 2
+            },
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+
+        })
+
+    })
+
+    it("doit empêcher l'ajout d'un produit en rupture de stock", () => {
+        cy.request({
+            method: "POST",
+            url: "http://localhost:8081/orders/add",
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+
+    })
+
+    it("doit ajouter un avis", () => {
+        cy.request({
+            method: "POST",
+            url: "http://localhost:8081/reviews",
+            body:
+            {
+                "title": "string",
+                "comment": "string",
+                "rating": 5
+            },
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+
+        })
+    })
+
+})
+
